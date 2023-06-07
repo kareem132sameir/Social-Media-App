4ce27486bb78ae5193e2282a203727f77b85a5b8
Mohamed Nasser <112254839+Mohamed17nasser@users.noreply.github.com>
2023-06-07 20:09:52 +0300

    Add files via upload

--- Helpers/tokenAuth.js
+++ Helpers/tokenAuth.js
@@ -1,12 +1,14 @@
 const jwt=require('jsonwebtoken');
 const AppError=require('../Helpers/AppError');
+const User=require('../Models/Users');
 
 
 const verifyToken=async(req,res,next)=>{
     const token=req.headers.authorization;
     if(!token) return next(new AppError('please provide a token'));
-    const user_id=jwt.verify(token,'mytoken');
-    const id=user_id.id;
+    const id=jwt.verify(token,'mytoken').id;
+    req.authorizedUser=await User.findById(id);
+    if(!req.authorizedUser) return next(new AppError('user is not found :/'));
     req.id=id;
     next();
 }
